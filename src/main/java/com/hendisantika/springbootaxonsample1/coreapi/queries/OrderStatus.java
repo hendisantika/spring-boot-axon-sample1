ad8d04e988ac793b350ceefb6a9afc7294e8654b
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:16:30 +0700

    #25 - Add OrderStatus

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/coreapi/queries/OrderStatus.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootaxonsample1.coreapi.queries;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.16
+ */
+public enum OrderStatus {
+
+    CREATED, CONFIRMED, SHIPPED
+}
