1be7432fe3619a14bb13dc690a9657da9fa29647
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:11:23 +0700

    #16 - Add OrderShippedEvent

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/coreapi/events/OrderShippedEvent.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootaxonsample1.coreapi.events;
+
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.11
+ */
+public class OrderShippedEvent {
+    private final String orderId;
+
+    public OrderShippedEvent(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrderShippedEvent other = (OrderShippedEvent) obj;
+        return Objects.equals(this.orderId, other.orderId);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderShippedEvent{" +
+                "orderId='" + orderId + '\'' +
+                '}';
+    }
+}
