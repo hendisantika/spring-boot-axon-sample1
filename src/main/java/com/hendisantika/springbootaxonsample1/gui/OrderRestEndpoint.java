5d02a8ecf119fc1aa2b2605295f4b492c2c8795b
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:18:10 +0700

    #27 - Add OrderRestEndpoint

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/gui/OrderRestEndpoint.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootaxonsample1.gui;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.17
+ */
+@RestController
+public class OrderRestEndpoint {
+
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
+
+    public OrderRestEndpoint(CommandGateway commandGateway, QueryGateway queryGateway) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+    }
+}
