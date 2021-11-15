3d1eea084c5593984e5e546b69943361021fdcd5
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:08:36 +0700

    #11 - Add DecrementProductCountCommand

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/coreapi/command/DecrementProductCountCommand.java
@@ -0,0 +1,58 @@
+package com.hendisantika.springbootaxonsample1.coreapi.command;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.08
+ */
+public class DecrementProductCountCommand {
+    @TargetAggregateIdentifier
+    private final String orderId;
+    private final String productId;
+
+    public DecrementProductCountCommand(String orderId, String productId) {
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DecrementProductCountCommand that = (DecrementProductCountCommand) o;
+        return Objects.equals(orderId, that.orderId) && Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, productId);
+    }
+
+    @Override
+    public String toString() {
+        return "DecrementProductCountCommand{" +
+                "orderId='" + orderId + '\'' +
+                ", productId='" + productId + '\'' +
+                '}';
+    }
+}
