63f54cae9a4520e25dc92f64757de8268c7163f4
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:10:55 +0700

    #15 - Add OrderCreatedEvent

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/coreapi/events/OrderCreatedEvent.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootaxonsample1.coreapi.events;
+
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.10
+ */
+public class OrderCreatedEvent {
+    private final String orderId;
+
+    public OrderCreatedEvent(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OrderCreatedEvent that = (OrderCreatedEvent) o;
+        return Objects.equals(orderId, that.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderCreatedEvent{" +
+                "orderId='" + orderId + '\'' +
+                '}';
+    }
+}
