25792527bdccffa3dc1ae6dbbc514703495d0e85
Hendi Santika <hendisantika@gmail.com>
2021-11-16 06:15:29 +0700

    #24 - Add FindAllOrderedProductsQuery

---
+++ src/main/java/com/hendisantika/springbootaxonsample1/coreapi/queries/FindAllOrderedProductsQuery.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootaxonsample1.coreapi.queries;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/21
+ * Time: 06.15
+ */
+public class FindAllOrderedProductsQuery {
+}
